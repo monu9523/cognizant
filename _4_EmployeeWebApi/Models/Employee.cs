89c6454736ea9cfc9c1a194ef6178ae97bd4f74f
Roshni Singh <singhroshni3211@gmail.com>
2026-07-07 21:06:46 +0530

    Revert "Add ASP .NET CORE WEB API projects"
    This reverts commit 95bf0263a016132475edc4a67fef092af9dc1625.

--- _4_EmployeeWebApi/Models/Employee.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Employee
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Department { get; set; }
-    public double Salary { get; set; }
-}
